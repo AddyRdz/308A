13e82dc9cccffcac40b33705f7b9ef6c87af37f4
Addy Rodriguez <rdz.33.a@gmail.com>
2024-09-04 22:18:12 -0500

    reformatted, got rid of buttons, added nav

--- business.js
+++ business.js
@@ -1,16 +1,13 @@
 import { API_KEY } from "./apiKey.js";
 
 // // business news div element.
-const getBusinessNews = document.getElementById("getBusinessNews");
-const getTopNews = document.getElementById("getTopNews");
-// get business news button
-const getBusinessNewsButton = document.getElementById("getBusinessNewsButton");
+const getBusinessNewsLink = document.getElementById("getBusinessNewsLink");
+const newsDisplay = document.getElementById("newsDisplay");
 
 // button refreshes when clicked
-getBusinessNewsButton.addEventListener("click", getTopBusinessNews);
+getBusinessNewsLink.addEventListener("click", getTopBusinessNews);
 
 async function getTopBusinessNews() {
-  getTopNews.innerHTML = "";
   const res = await fetch("https://newsapi.org/v2/everything?q=business", {
     headers: { "x-api-key": API_KEY },
   });
@@ -18,13 +15,13 @@ async function getTopBusinessNews() {
   const data = await res.json();
   console.log(data);
 
-  getBusinessNews.innerHTML = "";
+  newsDisplay.innerHTML = "";
 
   data.articles.forEach((article) => {
     const businessStory = document.createElement("div");
     businessStory.innerHTML = `<div id = topNews><h1>${article.title}</h1></div>
     <img src=${article.urlToImage}>
     <a href=${article.url}>Read More</a>`;
-    getBusinessNews.appendChild(businessStory);
+    newsDisplay.appendChild(businessStory);
   });
 }
--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 import { API_KEY } from "./apiKey.js";
 
 // the all top news div element.
-const getTopNews = document.getElementById("getTopNews");
+const newsDisplay = document.getElementById("newsDisplay");
 // get top us news button
-const getTopUsNewsButton = document.getElementById("getTopUsNewsButton");
+const topNewsLink = document.getElementById("topNewsLink");
 // button refreshes when clicked
-getTopUsNewsButton.addEventListener("click", getNews);
+topNewsLink.addEventListener("click", getNews);
 
 document.addEventListener("DOMContentLoaded", getNews);
 
 async function getNews() {
-  const getBusinessNews = document.getElementById("getBusinessNews");
-  getBusinessNews.innerHTML = "";
-
   const res = await fetch("https://newsapi.org/v2/top-headlines?country=us", {
     headers: { "x-api-key": API_KEY },
   });
 
   const data = await res.json();
   console.log(data);
-
-  getTopNews.innerHTML = "";
+  newsDisplay.innerHTML = "";
 
   data.articles.forEach((article) => {
     const story = document.createElement("div");
     story.innerHTML = `<div id = topNews><h1>${article.title}</h1></div>
     <img src=${article.urlToImage}>
     <a href=${article.url}>Read More</a>`;
-    getTopNews.appendChild(story);
+    newsDisplay.appendChild(story);
   });
 }
