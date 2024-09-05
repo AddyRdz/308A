df8d86eb5820a875fa660ef7361c7cf49cf8af18
Addy Rodriguez <rdz.33.a@gmail.com>
2024-09-05 15:22:49 -0500

    sports cards

--- sports.js
+++ sports.js
@@ -16,12 +16,19 @@ async function getSportsNews() {
   console.log(data);
 
   newsDisplay.innerHTML = "";
+  newsDisplay.className = "row";
 
   data.articles.forEach((article) => {
     const sportsStory = document.createElement("div");
-    sportsStory.innerHTML = `<div id = topNews><h1>${article.title}</h1></div>
-    <img src=${article.urlToImage}>
-    <a href=${article.url}>Read More</a>`;
+    sportsStory.className = "col-md-3 mb-4"
+    sportsStory.innerHTML = `<div class="card" style="width: 18rem;">
+ <div class="card" style="width;">
+  <img src="${article.urlToImage}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="${article.title}">${article.title}</h5>
+    <a href="${article.url}" class="btn btn-primary">Read More</a>
+  </div>
+</div>`
     newsDisplay.appendChild(sportsStory);
   });
 }
