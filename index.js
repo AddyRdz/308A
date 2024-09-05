19a4e464df771163f0a8cdfc01de3172d16d016c
Addy Rodriguez <rdz.33.a@gmail.com>
2024-09-05 14:28:42 -0500

    Can see cards

--- index.js
+++ index.js
@@ -20,9 +20,18 @@ async function getNews() {
 
   data.articles.forEach((article) => {
     const story = document.createElement("div");
-    story.innerHTML = `<div id = topNews><h1>${article.title}</h1></div>
+    story.innerHTML = `<div class="card" style="width: 18rem;">
+ <div class="card" style="width: 18rem;">
+  <img src="${article.urlToImage}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="${article.title}">${article.title}</h5>
+    <p class="card-text">${article.description}</p>
+    <a href="${article.url}" class="btn btn-primary">Read More</a>
+  </div>
+</div>
+    <div id = topNews><h1>${article.title}</h1></div>
     <img src=${article.urlToImage}>
-    <a href=${article.url}>Read More</a>`;
+    <a href=${article.url}>Read More</a>`
     newsDisplay.appendChild(story);
   });
 }
